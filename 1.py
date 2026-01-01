632bdc02826ef9b4a274d700797cc7159e508d7d
MP Web Developer <mprwdev@gmail.com>
2026-01-01 13:02:15 +0200

    merge operator for dict

--- 1.py
+++ 1.py
@@ -2,8 +2,8 @@
 dict1 = {'a': 1, 'b': 2}
 dict2 = {'b': 3, 'c': 4}
 
-# merged_dict = dict1 | dict2
-# print(merged_dict)
+merged_dict = dict1 | dict2
+print(merged_dict)
 
-dict1 |= dict2
-print(dict1)
+# dict1 |= dict2
+# print(dict1)
