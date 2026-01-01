4c4f2a0c9c066d298c5fe96c36ae5e0e4743e5a0
MP Web Developer <mprwdev@gmail.com>
2026-01-01 22:30:05 +0200

    functions intro

--- 1.py
+++ 1.py
@@ -1,16 +1,8 @@
 
-visits = {'Monday': 5000,
-          'Tuesday': 3000,
-          'Wednesday': 4000,
-          'Thursday': 4500,
-          'Friday': 5000,
-          'Saturday': 2000,
-          'Sunday': 1500
-					}
+def my_function():
+	print('Hello World!')
+	x = 10
+	print(x ** 2)
 
-## YOUR CODE STARTS HERE
-total_visits = sum(visits.values())
-print(total_visits)
-
-percentage = {d: (v / total_visits) * 100 for d, v in visits.items()}
-print(percentage)
+my_function()
+my_function()
