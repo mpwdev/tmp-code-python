e78625a931e7a0731eeb46824d269162e2994d16
MP Web Developer <mprwdev@gmail.com>
2026-01-01 13:49:45 +0200

    total visits percent

--- 1.py
+++ 1.py
@@ -1,29 +1,16 @@
 
-# names = {'tom', 'ANNE', 'John', 'dAn'}
-# print(id(names))
-# names = {n.capitalize() for n in names}
-# print(id(names))
-# print(names)
+visits = {'Monday': 5000,
+          'Tuesday': 3000,
+          'Wednesday': 4000,
+          'Thursday': 4500,
+          'Friday': 5000,
+          'Saturday': 2000,
+          'Sunday': 1500
+					}
 
-# d1 = {'a': 1, 'b': 2, 'c': 3}
-# d2 = {k*2: v*2 for k,v in d1.items()}
-# print(d2)
+## YOUR CODE STARTS HERE
+total_visits = sum(visits.values())
+print(total_visits)
 
-# d3 = {k.upper(): v*2 for k,v in d1.items()}
-# print(d3)
-
-# d4 = {k.upper(): v*2 for k,v in d1.items() if v % 3 == 0}
-# print(d4)
-
-years = [2020, 2021, 2022]
-revenues = [10000, 50000, 10000]
-z = zip(years, revenues)
-#print(z)
-sales = list(z)
-print(sales)
-
-sales_dict = dict(zip(years, revenues))
-print(sales_dict)
-
-profit = {k: v*0.15 for k,v in sales_dict.items()}
-print(profit)
+percentage = {d: (v / total_visits) * 100 for d, v in visits.items()}
+print(percentage)
