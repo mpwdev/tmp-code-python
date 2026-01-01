51f7a59fd53eaebf1be9549e01b10ff8f2b4c41e
MP Web Developer <mprwdev@gmail.com>
2026-01-01 22:42:02 +0200

    multiline docstring

--- 1.py
+++ 1.py
@@ -1,6 +1,9 @@
 
 def say_hello(name):
-	"""This is custom function description - docstring"""
+	"""This is custom function description - docstring
+	
+	then parameter
+	the function"""
 	print(f'Hello, {name}')
 	
 say_hello('Dan')
