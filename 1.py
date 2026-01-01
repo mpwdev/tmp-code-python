eb13d3435c380aa13ba4c7f15f58d98c589a082d
MP Web Developer <mprwdev@gmail.com>
2026-01-01 13:08:13 +0200

    update dict operator

--- 1.py
+++ 1.py
@@ -2,11 +2,16 @@
 dict1 = {'a': 1, 'b': 2}
 dict2 = {'b': 3, 'c': 4}
 
-merged_dict = dict1 | dict2
-print(merged_dict)
+# merged_dict = dict1 | dict2
+# print(merged_dict)
+# print(id(dict1))
+# print(id(dict2))
+# print(id(merged_dict))
+
+print(f'{id(dict1)} - id dict1 before merge')
+print(f'{dict1} - dict1 before merge')
+dict1 |= dict2
+print(dict1)
+print(dict2)
 print(id(dict1))
 print(id(dict2))
-print(id(merged_dict))
-
-# dict1 |= dict2
-# print(dict1)
