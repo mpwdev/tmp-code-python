c5fb00e687027c99a302ce0ad8446dfc2e343151
MP Web Developer <mprwdev@gmail.com>
2026-01-02 14:18:51 +0200

    lambda funcions

--- 1.py
+++ 1.py
@@ -1,11 +1,3 @@
 
-def my_function(**kwargs):
-	print(kwargs)
-	for k, v in kwargs.items():
-		print(f'k is {k} an v is {v}')
-
-my_function(name='Bob', age=30, location = 'London', job='dev')
-
-person = {'name':'Bobby', 'age':35, 'location': 'Berlin', 'job':'devvv'}
-my_function(**person)
-
+square = lambda x: x **2
+print(square(5))
