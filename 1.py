ea2d6489c40a0a38c4061c59382ee9aa04ab82ae
MP Web Developer <mprwdev@gmail.com>
2026-01-02 12:38:30 +0200

    vat percentage calc func

--- 1.py
+++ 1.py
@@ -1,11 +1,14 @@
 
-def say_hello(name):
-	"""This is custom function description - docstring
+# Write a function called get_vat() with 2 parameters, price and vat percentage
+# It calculates and returns the VAT (value-added tax).
+# YOUR CODE STARTS HERE
+
+def get_vat(price, vat_percentage):
+	vat = price * (vat_percentage / 100)
+	return vat
 	
-	then parameter
-	the function"""
-	print(f'Hello, {name}')
-	
-say_hello('Dan')
-help(say_hello)
-print(say_hello.__doc__)
+
+
+# Capture the returned value in a new variable called vat.
+vat = get_vat(100, 20)
+print(f' vat is: {vat} %')
