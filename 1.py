eb8185c21b48d5c3ae053f42717a13b151373142
MP Web Developer <mprwdev@gmail.com>
2026-01-02 20:51:29 +0200

    tell and seek files cursor

--- 1.py
+++ 1.py
@@ -1,8 +1,26 @@
 
-#f = open('settings.txt')
-#f = open('./settings.txt')
-f = open('dir1/script1.py')
-print(f.closed)
-f.close()
-print(f.closed)
+# f = open('configuration.txt')
+# content = f.read(5)
+# print(content)
 
+# content = f.read(3)
+# print(content)
+
+# print(f.tell())
+
+# f.seek(2)
+# content = f.read(3)
+# print(content)
+# print(f.tell())
+
+# f.close()
+
+f = open('configuration.txt')
+print(f.read())
+f.close()
+
+f = open('configuration.txt')
+print('#' * 20)
+#f.seek(0)
+print(f.read())
+f.close()
