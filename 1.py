27eea65452c5a241f7e2bff4c771f0cecfb899df
MP Web Developer <mprwdev@gmail.com>
2026-01-02 15:20:57 +0200

    function list to uniq list

--- 1.py
+++ 1.py
@@ -1,3 +1,11 @@
 
-square = lambda x: x **2
-print(square(5))
+list_orig = [1,2,3,3,3,3,4,5, 1, 3, 5, 5, 5]
+
+def list_uniq(list_to_uniq):
+	t1 = set(list_to_uniq)
+	t2 = list(t1)
+	return t2
+
+n = list_uniq(list_orig)
+
+print(n)
