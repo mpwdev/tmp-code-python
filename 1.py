1ad8d2797aba018c9633c12612eb96c1747d8b30
MP Web Developer <mprwdev@gmail.com>
2026-01-04 21:09:30 +0200

    test ip ping example

--- 1.py
+++ 1.py
@@ -1,5 +1,15 @@
 
 import subprocess
-command = 'ping -n 1 8.8.8.8'
-output = subprocess.check_output(command.split())
-print(output.decode())
+
+with open('hosts.txt') as f:
+	ips = f.read().splitlines()
+	for ip in ips:
+		try:
+			command = f'ping -n 1 {ip}'
+			output = subprocess.check_output(command.split())
+			print(output.decode())
+		except Exception as e:
+			print(f'Error pinging {ip}: {e}')
+		print('=' * 20)
+
+print(ips)
