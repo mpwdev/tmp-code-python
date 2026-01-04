e639ff8af806a7a3c2c60e8db4e1c1d04b562754
MP Web Developer <mprwdev@gmail.com>
2026-01-04 21:19:50 +0200

    python oop

--- 1.py
+++ 1.py
@@ -1,15 +1,7 @@
 
-import subprocess
+class RobotTest:
+	"""Test class for robot functionality"""
+	pass
 
-with open('hosts.txt') as f:
-	ips = f.read().splitlines()
-	for ip in ips:
-		try:
-			command = f'ping -n 1 {ip}'
-			output = subprocess.check_output(command.split())
-			print(output.decode())
-		except Exception as e:
-			print(f'Error pinging {ip}: {e}')
-		print('=' * 20)
-
-print(ips)
+r1 = RobotTest()
+print(r1.__doc__)
