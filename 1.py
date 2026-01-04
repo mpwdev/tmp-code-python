155323eb2f415a1ee7c979754b037f383b083c64
MP Web Developer <mprwdev@gmail.com>
2026-01-04 22:13:48 +0200

    del method

--- 1.py
+++ 1.py
@@ -1,7 +1,14 @@
 
 class RobotTest:
 	"""Test class for robot functionality"""
-	pass
+	def __init__(self, name="Robo", year=2024):
+		self.name = name
+		self.year = year
+	
+	def __del__(self):
+		print(f'Robot {self.name} is being destroyed')
 
-r1 = RobotTest()
-print(r1.__doc__)
+r1 = RobotTest('R1', 2025)
+#print(r1.__doc__)
+print(f'Robot name: {r1.name}')
+print(r1.__dict__)
