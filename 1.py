8d443285a00b3ca45abcc5505f91c455511d076f
MP Web Developer <mprwdev@gmail.com>
2026-01-05 19:22:01 +0200

    import modules

--- 1.py
+++ 1.py
@@ -1,5 +1,12 @@
+# import random
 
-# Total number of IPv6 (each ipv6 consists of 128 bits)
-no_of_ipv6 = 2 ** 128
-no_of_digits = len(str(no_of_ipv6))
-print(no_of_digits)     # => 39
+# print(random.randint(1, 100))
+# print(random.choice(['apple', 'banana', 'cherry']))
+
+# from random import randint, choice
+# print(randint(1, 100))
+# print(choice(['apple', 'banana', 'cherry']))
+
+# import random as rnd
+# print(rnd.randint(1, 100))
+# print(rnd.choice(['apple', 'banana', 'cherry']))
