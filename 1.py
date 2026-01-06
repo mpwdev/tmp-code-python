c4f7e6b3ab9886e275b22b49542021643d7dd57a
MP Web Developer <mprwdev@gmail.com>
2026-01-06 21:11:53 +0200

    sys module

--- 1.py
+++ 1.py
@@ -1,1 +1,9 @@
-print('hello Max')
+import sys
+# print(sys.version)
+# print(sys.implementation)
+# print(sys.platform)
+# print(sys.path)
+print(f'Number of arguments: {len(sys.argv)}')
+print(f'This is name of the script: {sys.argv[0]}')
+print(f'The script arguments are: {sys.argv}')
+print(f'The script arguments are: {sys.argv[1:]}')
