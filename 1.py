88a0dc19df41b91e08fba129e6a095c139ef25ff
MP Web Developer <mprwdev@gmail.com>
2026-01-06 19:26:03 +0200

    python venv settings

--- 1.py
+++ 1.py
@@ -1,12 +1,1 @@
-# import random
-
-# print(random.randint(1, 100))
-# print(random.choice(['apple', 'banana', 'cherry']))
-
-# from random import randint, choice
-# print(randint(1, 100))
-# print(choice(['apple', 'banana', 'cherry']))
-
-# import random as rnd
-# print(rnd.randint(1, 100))
-# print(rnd.choice(['apple', 'banana', 'cherry']))
+print('hello Max')
