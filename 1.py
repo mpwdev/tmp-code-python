08ec81a97cc4798a84a047838bdb4ddef062d444
MP Web Developer <mprwdev@gmail.com>
2026-01-07 21:12:49 +0200

    python json serialization

--- 1.py
+++ 1.py
@@ -1,2 +1,8 @@
-import random
-print(random.randint(4, 10))
+import json
+friends = {'Dan': [20, 'London', 3234342], 'Maria': [22, 'New York', 9876543]}
+
+with open('friends.json', 'w') as f:
+	json.dump(friends, f)
+
+	json_string = json.dumps(friends, indent=4)
+	print(json_string)
