d722ee107d61279427f07f2c23450046c39dece4
MP Web Developer <mprwdev@gmail.com>
2026-01-07 18:57:16 +0200

    copy file with shutil module

--- 1.py
+++ 1.py
@@ -1,14 +1,2 @@
-import os
-os.chdir('./dir2/')
-
-i = 1
-for file in os.listdir():
-	#print(file)
-	file_name, file_extension = os.path.splitext(file)
-	#print(f'File Name: {file_name}, File Extension: {file_extension}')
-	new_file_name = f'{str(i)}_{file_name}{file_extension}'
-	print(new_file_name)
-	i += 1
-
-	print(f'Renaming {file} to {new_file_name}')
-	os.rename(file, new_file_name)
+import shutil
+shutil.copyfile('dir2/1_a.txt', 'dir1/1_a_new.txt')
