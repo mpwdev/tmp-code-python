fd9bbd34e967ee1119d2abe987c62fefee723024
MP Web Developer <mprwdev@gmail.com>
2026-01-09 21:16:28 +0200

    @ syntax

--- 1.py
+++ 1.py
@@ -1,18 +1,34 @@
 
 user = {'username': 'js', 'level': 'admin33'}
 
-# decorator example
-def user_has_permission(func):
-	if user['level'] == 'admin':
-		return func
-	else:
-		return None
-	
+def only_admin(func):
+	def wrapper_only_admin(*args, **kwargs):
+		if user['level'] == 'admin':
+			return func(*args, **kwargs)
+		else:
+			raise PermissionError("User does not have admin privileges")
+	return wrapper_only_admin
+
+# # decorator example
+# def user_has_permission(func):
+# 	def wrapper_user_has_permission():
+# 		if user['level'] == 'admin':
+# 			return func()
+# 		else:
+# 			return None
+# 	return wrapper_user_has_permission
+
+@only_admin # @ syntax
 def show_pass():
 	return "password123"
 
-my_function = user_has_permission(show_pass)
-if my_function != None:
-	print(my_function())
-else:
-	print('No access')
+# try:
+# 	my_function = only_admin(show_pass)
+# 	print(my_function())
+# except PermissionError as e:
+# 	print(e)
+
+try:
+	show_pass()
+except PermissionError as e:
+	print(e)
