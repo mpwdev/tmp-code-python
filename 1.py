8317c142af319648a4474c0b181782d62af7e1c9
MP Web Developer <mprwdev@gmail.com>
2026-01-11 12:40:01 +0200

    csv file write

--- 1.py
+++ 1.py
@@ -1,18 +1,12 @@
 import csv
 
-with open('airtravel.csv', 'r') as f:
-	reader = csv.reader(f)
-	next(reader) # skip first line
-	year_1958 = dict()
-	for row in reader:
-		#print(row)
-		year_1958[row[0]] = row[1]
-	
-	#print(year_1958)
+# with open('people.csv', 'a') as csvfile:
+# 	writer = csv.writer(csvfile)
+# 	csvdata = (10, 'Bobby', 'Spain')
+# 	writer.writerow(csvdata)
 
-	max_1958 = max(year_1958.values())
-	#print(max_1958)
-
-	for k, v in year_1958.items():
-		if max_1958 == v:
-			print(f'1958 month: {k}, flights: {v.strip()}')
+with open('numbers.csv', 'w', newline='') as f:
+	writer = csv.writer(f)
+	writer.writerow(['x', 'x**2', 'x**3', 'x**4'])
+	for x in range(1, 11):
+		writer.writerow([x, x**2, x**3, x**4])
