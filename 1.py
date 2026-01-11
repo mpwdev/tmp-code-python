188189dc90113370cc21e655e496aa19ffc7da80
MP Web Developer <mprwdev@gmail.com>
2026-01-11 13:12:20 +0200

    csv dialects

--- 1.py
+++ 1.py
@@ -1,6 +1,21 @@
 import csv
 
-with open('passwd.csv', 'r') as f:
-	reader = csv.reader(f, delimiter=':', lineterminator='\n')
+# with open('passwd.csv', 'r') as f:
+# 	reader = csv.reader(f, delimiter=':', lineterminator='\n')
+# 	for row in reader:
+# 		print(row)
+
+# print(csv.list_dialects())
+
+csv.register_dialect('hashes', delimiter='#', quoting=csv.QUOTE_NONE, lineterminator='\n')
+
+with open('items.csv', 'r') as csvfile:
+	reader = csv.reader(csvfile, dialect='hashes')
+
 	for row in reader:
 		print(row)
+
+with open('items.csv', 'a') as csvfile:
+	writer = csv.writer(csvfile, dialect='hashes')
+	writer.writerow(('book', 5, 2.5))
+
