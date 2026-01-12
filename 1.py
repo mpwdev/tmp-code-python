c13928ca25aa170e0ac77096d0fa08cd0cc85217
MP Web Developer <mprwdev@gmail.com>
2026-01-12 22:30:33 +0200

    excel read variants

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
 import openpyxl
 
-wb = openpyxl.load_workbook('store.xlsx')
+wb = openpyxl.load_workbook('store.xlsx', data_only=True)
 
 #print(wb.sheetnames)
 
@@ -13,19 +13,41 @@ sheet = wb['Products']
 b2_cell = sheet['B2']
 c2_cell = sheet['c2'] # case insensitive
 
-print(b2_cell.value, c2_cell.value)
+# print(b2_cell.value, c2_cell.value)
 
-print(sheet.cell(row=4, column=2).value)
+# print(sheet.cell(row=4, column=2).value)
 
-print(c2_cell.row, c2_cell.column)
+# print(c2_cell.row, c2_cell.column)
 
-print(sheet['A5'].data_type)
-print(sheet['B5'].data_type)
-print(sheet['A5'].encoding)
+# print(sheet['A5'].data_type)
+# print(sheet['B5'].data_type)
+# print(sheet['A5'].encoding)
 
-print(sheet['D4'].parent)
+# print(sheet['D4'].parent)
 
-print(dir(b2_cell))
+# print(dir(b2_cell))
 
-print(b2_cell.style, b2_cell._style, c2_cell.coordinate, c2_cell.protection)
+# print(b2_cell.style, b2_cell._style, c2_cell.coordinate, c2_cell.protection)
+
+# cell_rande = sheet['B2:C11']
+# for product, price in cell_rande:
+# 	print(f'product: {product.value}, price: {price.value}$')
+
+print(f'Sheet Dimentions: {sheet.dimensions}')
+print(sheet.max_row, sheet.max_column)
+
+for a, b, c, d, e in sheet[sheet.dimensions]:
+	print(a.value, b.value, c.value, d.value, e.value)
+
+print('-' * 30)
+
+for row in sheet.rows:
+	for cell in row:
+		print(f'{cell.value} ', end='')
+	print('')
+
+print('=' * 30)
+
+for row in sheet.values:
+	print(row) # will print row as tuple
 
