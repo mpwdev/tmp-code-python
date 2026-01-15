9284bbb47e824b3eb1995640a863012490a4f85e
MP Web Developer <mprwdev@gmail.com>
2026-01-15 19:20:36 +0200

    CRUD operations with sheet

--- 1.py
+++ 1.py
@@ -1,11 +1,30 @@
 import openpyxl
 
 wb = openpyxl.load_workbook('store.xlsx')
-sheet = wb.active
-sheet['e3'] = '=c3*d3'
+print(wb.sheetnames)
 
-for c, d, e in sheet['c2:e12']:
-	e.value = f'={c.coordinate}*{d.coordinate}'
+sheet = wb['Products']
+
+# print(dir(sheet))
+
+print(sheet.title)
+# sheet.title = 'Products New'
+# print(sheet.title)
+
+# print(sheet.sheet_format)
+# print(sheet.sheet_properties)
+
+#wb.create_sheet('NewSheet1', 0)
+
+# print(wb.sheetnames)
+# sheet_to_remove = wb['NewSheet11']
+# wb.remove(sheet_to_remove)
+# print(wb.sheetnames)
+
+# source = wb['NewSheet1']
+# destination = wb.copy_worksheet(source)
+# print(destination.title)
+
 
 wb.save('store.xlsx')
 
