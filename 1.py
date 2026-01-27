6dd355b338624262971d7252691dfc0f257a0cbe
MP Web Developer <mprwdev@gmail.com>
2026-01-27 22:21:50 +0200

    var names snake_case

--- 1.py
+++ 1.py
@@ -1,19 +1,2 @@
-import openpyxl
-from openpyxl.styles import *
-
-wb = openpyxl.load_workbook('store.xlsx')
-sheet = wb.active
-
-my_cell = sheet['B4']
-
-# print(dir(openpyxl.styles))
-
-font = Font(name="Tahoma", size=16, color=colors.BLUE, bold=True, italic=True)
-my_cell.font = font
-
-
-
-
-wb.save('store.xlsx')
-
-
+max_value = 99
+print(max_value)
