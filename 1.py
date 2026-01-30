ed7ebbc4125bd711d817a0e8b81597de6fb54ee4
MP Web Developer <mprwdev@gmail.com>
2026-01-30 23:02:02 +0200

    floating point int

--- 1.py
+++ 1.py
@@ -1,6 +1,15 @@
 
-a = 0.1 * 3
-b = 0.3
-print(a == b)  # This will print False due to floating-point precision issues
-print(format(a, '0.2f') == format(b, '0.2f'))  # This will print True
-print(round(a, 2) == round(b, 2))  # This will also print True
+from math import isclose
+# isclose()
+x = 0.0000001
+y = 0.0000002
+print(x == y)
+print(isclose(x, y, abs_tol=0.01))
+
+a = 9999999999.01
+b = 9999999999.02
+print(isclose(a, b, rel_tol=0.01))
+
+a = 3.4
+b = 2.3
+print(format(a+b, '.2f'))
