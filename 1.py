9fe44d7c4e80b9553839a25f8817dabb3c94d120
MP Web Developer <mprwdev@gmail.com>
2026-01-30 21:19:58 +0200

    floating point numbers v1

--- 1.py
+++ 1.py
@@ -1,5 +1,6 @@
-a, b = 5, 6
-print(a is b)
-print(a is not b)
 
-
+a = 0.1 * 3
+b = 0.3
+print(a == b)  # This will print False due to floating-point precision issues
+print(format(a, '0.2f') == format(b, '0.2f'))  # This will print True
+print(round(a, 2) == round(b, 2))  # This will also print True
