2f560908e7a79fcc8134dbcfb709391ed6ed43f8
MP Web Developer <mprwdev@gmail.com>
2026-02-04 20:01:56 +0200

    input basic

--- 1.py
+++ 1.py
@@ -1,15 +1,4 @@
-
-from math import isclose
-# isclose()
-x = 0.0000001
-y = 0.0000002
-print(x == y)
-print(isclose(x, y, abs_tol=0.01))
-
-a = 9999999999.01
-b = 9999999999.02
-print(isclose(a, b, rel_tol=0.01))
-
-c = 4.5
-d = 6.7
-print(format(c + d, '.2f'))
+price = input('Enter the price of the item: ')
+quantity = input('Enter the quantity of the item: ')
+total = float(price) * int(quantity)
+print(f'The total cost of the item is {total}')
